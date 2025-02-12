deaca66b232121b7366dc7873682267b3613c3bc
Abhi5241 <chauhanabhishek611@gmail.com>
2025-02-12 12:17:08 +0530

    machine learning pipline created

---
+++ start_training.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.pipeline
+def start_training():
+    try:
+        pass
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
