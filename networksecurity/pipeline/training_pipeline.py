d232986a6ef4cc07274b78b8d8b2948e6be530c3
Abhi5241 <chauhanabhishek611@gmail.com>
2025-02-17 07:52:44 +0530

    data ingestion code is done

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -1,10 +1,14 @@
 import os
 import sys
 
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.components.data_evaluation import ModelEvaluation 
 from networksecurity.components.model_pusher import ModelPusher
 
 
@@ -14,10 +18,12 @@ from networksecurity.entity.config_entity import(
     DataValidationConfig,
     DataTransformationConfig,
     ModelTrainerConfig,
+    ModelEvaluationConfig,
     ModelPusherConfig
-) 
+   
+)
 
-from networksecurity.entity.artifact_entity import(
+from networksecurity.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
     DataTransformationArtifact,
@@ -26,50 +32,52 @@ from networksecurity.entity.artifact_entity import(
     ModelPusherArtifact
 )
 
-from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.logger.logger import logging
-
 class TrainingPipeline:
-    def __init__(self, pipeline_config: TrainingPipelineConfig):
-        self.pipeline_config = pipeline_config
-
+    def __init__(self):
+        pass
+        
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_validation(self):
+    def start_data_validation():
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_transformation(self):
+    def start_data_transformation():
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_trainer(self):
+    def start_model_trainer():
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_evaluation(self):
+    def start_model_evaluation():
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_pusher(self):
+    def start_model_pusher():
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e,sys)
- 
-    def run_pipeline(self):
+        
+    def run_pipeline():
         try:
             pass
         except Exception as e:
